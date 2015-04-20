49eb7dc3cf5044a0031103289b37948326fab82a
Jan Pavlica <janpavlica@yahoo.com>
2015-04-20 18:22:53 +0200

    2.naloga

---
+++ ugani_stevilo.py
@@ -0,0 +1,9 @@
+stevilo = 4
+
+ugib = raw_input("Ugani skrito stevilo. Ugibaj zdaj: ")
+if ugib == 4:
+    print"Bravo, uganil,si!"
+if ugib > 4:
+    print"stevilo je manjse"
+else:
+    print"stevilo je vecje"
